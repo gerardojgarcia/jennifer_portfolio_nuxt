22614efa5ae84a99ab69bd3c9654ba84d5417e70
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-03-21 12:32:50 -0700

    first commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div id="footer">
+<strong> this is the footer</strong>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+#footer {
+  height: 5vh;
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "index.vue"
+}
+</script>
+
+<template>
+  <div id='hero' class="h-screen">
+<h1>Hello World</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+#hero {
+  height: 95vh;
+}
+</style>
